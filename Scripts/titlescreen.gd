e47604982cde0fd0a365256573ae3f76dfcb5383
retroisbest <pfl@st-mary.blackpool.sch.uk>
2024-05-16 09:59:42 +0100

    Wall collision fixed
    attached wall.gd to wall.tscn (duh!)
    changed title music

--- Scripts/titlescreen.gd
+++ Scripts/titlescreen.gd
@@ -24,7 +24,6 @@ func _process(_delta):
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
-
 func _on_quit_pressed():
 	get_tree().quit()
 
--- Scripts/wall.gd
+++ Scripts/wall.gd
@@ -2,11 +2,3 @@ extends RigidBody2D
 class_name Wall
 signal ball_collison
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
